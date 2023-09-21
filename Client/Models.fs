a12437290fba4d2ed2f0dc3a1d1263d2c1f48a3c
weebs <147543+weebs@users.noreply.github.com>
2023-09-21 14:54:22 -0500

    float3f + world state

--- Client/Models.fs
+++ Client/Models.fs
@@ -2,8 +2,13 @@ module Dootverse.Models
 
 open Browser.Types
 
+type float3f = { X: float; Y: float; Z: float }
 type float2f = { X: float; Y: float }
-type GameWorldState = { playerPosition: float2f; playerRotation: float }
+
 type Asset =
     Image of ImageData
+    
+    
+type GameWorldState = { playerPosition: float2f; playerRotation: float; entities: (float2f * Asset)[] }
+    
 type Game = { Assets: Map<string, Asset> }
