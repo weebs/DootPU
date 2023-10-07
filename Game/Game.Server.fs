17dc34b9da954b83e2a49c86e1bc7e58cf33c413
David Thorpe <dave.j.thorpe@gmail.com>
2023-10-06 22:30:39 -0500

    dedicated server

---
+++ Game/Game.Server.fs
@@ -0,0 +1,32 @@
+﻿module Dootverse.Game.Server
+
+open Fable.Core
+open Browser
+open Dootverse.Client.JsImports
+open Dootverse.Client.Game
+open Dootverse
+open Thoth.Json
+
+let start () = promise {
+    do! RAPIER.init ()
+    let c = WebSocket.Create "ws://127.0.0.1:8000/ws"
+    c.onopen <- fun ev ->
+        let server = GameServer(
+            RAPIER.World.Create(RAPIER.Vector3.Create(0, -9.81, 0)),
+            Network.Scene.createScene ()
+        )
+        c.onmessage <- fun ev ->
+            match Decode.Auto.fromString<Network.LobbyConnection.ServerMessage> (string ev.data) with
+            | Ok message ->
+                match message with
+                | Network.LobbyConnection.ServerMessage.ConnectionRequest (clientId, webRtcRequest) ->
+                    promise {
+                        let! response = server.AnswerRequest webRtcRequest
+                        c.send (Encode.Auto.toString (Network.LobbyConnection.ClientMessage.ConnectionResponse (clientId, response)))
+                    } |> ignore
+                | _else ->
+                    printfn "%A" _else
+            | error -> console.log error
+            
+        c.send (Encode.Auto.toString (Network.LobbyConnection.ClientMessage.HostLobby "w00tcamp"))
+}
