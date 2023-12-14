510f9fb7bdc0ccf84c7b8f1db487409eac54287c
weebs <147543+weebs@users.noreply.github.com>
2023-12-14 10:30:09 -0600

    debug file + vite reverse proxy setting

--- Game/server.js
+++ Game/server.js
@@ -1,10 +1,19 @@
 ﻿const { chromium, devices } = await import('playwright')
-const browser = await chromium.launch({ headless: true })
-const page = await browser.newPage()
-page.on('console', msg => {
-    console.log(msg)
-})
-const game = await chromium.launch({ headless: false })
-const gamePage = await game.newPage()
-await page.goto('http://localhost:5173/dedicated_server.html')
-await gamePage.goto('http://localhost:5173/game.html')
+const { spawn } = await import('node:child_process')
+const fableWatch = spawn('dotnet', ['fable', 'watch'])
+const dotnetSignalingServer = spawn('dotnet', ['run', '--project', '../Signaling.Server/Signaling.Server.fsproj'])
+const vite = spawn('cmd.exe', ['/c', 'npx', 'vite', '--host'])
+fableWatch.stdout.on('data', data => console.log(data))
+dotnetSignalingServer.stdout.on('data', data => console.log(data))
+vite.stdout.on('data', data => console.log(data))
+setTimeout(async () => {
+    const serverBrowser = await chromium.launch({headless: true})
+    const serverPage = await serverBrowser.newPage()
+    serverPage.on('console', msg => {
+        console.log("Server: ", msg._event)
+    })
+    const gameBrowser = await chromium.launch({headless: false})
+    const gamePage = await gameBrowser.newPage()
+    await serverPage.goto('http://localhost:5173/dedicated_server.html')
+    await gamePage.goto('http://localhost:5173/game.html')
+}, 4000);
--- Game/vite.config.ts
+++ Game/vite.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/ws': {
-                target: 'ws://localhost:8000',
+                target: 'ws://127.0.0.1:8000',
                 ws: true
             }
         }
